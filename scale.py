20f95f8db8ec6b38f567e0997a4a4429f20313f2
Kevin Kays <kkays@raybeam.com>
2021-02-01 01:54:38 +0000

    can light led!

--- scale.py
+++ scale.py
@@ -5,11 +5,11 @@ import time
 from hx711 import HX711
 import RPi.GPIO as GPIO
 
-MENU_TEXT = """
-What would you like to do?
-1. Zero the scale.
-2. Read forever.
-"""
+LED_PINS = {
+  'RED': 16,
+  'GREEN': 20,
+  'BLUE': 21,
+}
 
 
 class Scale:
@@ -28,14 +28,14 @@ class Scale:
     rounded = round(mean, -3)
     if not zeroed:
       return rounded
-    return rounded -self.zero
+    return rounded - self.zero
 
 
-def main():
+def main(led_pins):
   try:
     scale = Scale()
     while True:
-      menu(scale)
+      menu(scale, led_pins)
   except KeyboardInterrupt:
     print('Goodbye!')
     return 0
@@ -43,15 +43,33 @@ def main():
     GPIO.cleanup()
 
 
-def menu(scale):
-  choice = input(MENU_TEXT)
+def menu(scale, led_pins):
+  menu_text = ('What would you like to do?\n'
+               '1. Zero the scale.\n'
+               '2. Read forever.\n'
+               '3. Light led.\n')
+  choice = input(menu_text)
+
   if choice == '1':
     zero_scale(scale)
     return
   if choice == '2':
     read_forever(scale)
     return
+  if choice == '3':
+    cycle_leds(led_pins)
+    return
   print('Invalid choice!')
+
+
+def cycle_leds(led_pins):
+  print('lighting LEDs')
+  for color, pin in led_pins.items():
+    print(f'Lighting: {color}')
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(pin, GPIO.LOW)
+  print('Lit all colors.')
 
 
 def read_forever(scale):
@@ -70,6 +88,13 @@ def zero_scale(scale):
   print(f'Zeroed to {scale.zero}!')
 
 
+def setup(led_pins):
+  GPIO.setmode(GPIO.BCM)
+  for pin in led_pins.values():
+    GPIO.setup(pin, GPIO.OUT)
+  cycle_leds(led_pins)
+
 
 if __name__ == "__main__":
-  main()
+  setup(LED_PINS)
+  main(LED_PINS)
